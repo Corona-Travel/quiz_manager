e91f604f930a44926ddd9d833e2adc86d68cda31
helena2505 <elenakarelina833@gmail.com>
2021-12-11 19:37:51 +0300

    Get quiz from resource

--- quiz_manager/app.py
+++ quiz_manager/app.py
@@ -5,19 +5,22 @@ from typing import Any, Awaitable, Callable
 from fastapi import FastAPI, Depends, HTTPException, status
 import httpx
 
-from .types import QuizWithAnswer, QuizWithScore
+from .types import QuizWithAnswer, QuizWithScore, QuizesWithoutAnswer
 from .settings import Settings
 
 app = FastAPI(openapi_tags=[{"name": "service:quiz_manager"}])
 
-@app.get("/quiz_manager/{quiz_id}", response_model=QuizWithoutAnswer)
+@app.get("/quiz_manager/{quiz_id}", response_model=QuizesWithoutAnswer)
 async def get_quiz(quiz_id: str):
     async with httpx.AsyncClient() as client:
         res = loads((await client.get(f"{settings.quizzes_url}/quizzes/{quiz_id}")).text)
+        for question in res.questions:
+            for answ in question.answer:
+                del answ.correct
         return res
 
 @app.post("/quiz_manager/{quiz_id}", response_model=QuizWithScore, tags="service:quiz_manager")
-async def check_quiz():
+async def check_quiz(submitted_quiz: QuizWithAnswer):
     async with httpx.AsyncClient() as client:
         res = loads((await client.get(f"{settings.quizzes_url}/quizzes")).text)
         return res
